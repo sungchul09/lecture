20d9b1e96d076e3f356d87a2948c40745d935a8c
sungchul09 <sungchul09@naver.com>
2023-01-24 16:44:51 +0900

    14.14 모든 섹션들 구현하기💡 (유튜브 URL 만들기편)

--- typescript-ellie/motion/src/components/item/video.ts
+++ typescript-ellie/motion/src/components/item/video.ts
@@ -11,8 +11,23 @@ export class VideoComponent extends BaseComponent<HTMLElement> {
         `)
 
         const iframe = this.element.querySelector('.iframe')! as HTMLIFrameElement
-        iframe.src = url
+        
+        iframe.src = this.convertToEmbeddedURL(url)
+
         const titleElement = this.element.querySelector('.video__title')! as HTMLHeadingElement
         titleElement.textContent = title
+    }
+
+    private convertToEmbeddedURL(url: string): string {
+        const regExp = /^(?:https?:\/\/)?(?:www\.)?(?:(?:youtube.com\/(?:(?:watch\?v=)|(?:embed\/))([a-zA-Z0-9-]{11}))|(?:youtu.be\/([a-zA-Z0-9-]{11})))/;
+        const match = url.match(regExp)
+
+        const videoId = match ? match[1] || match[2] : undefined
+
+        if(videoId) {
+            return `https://www.youtube.com/embed/${videoId}`
+        }
+
+        return url
     }
 }
