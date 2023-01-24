08d65c1a63ea53355a0c40ef9650759132c2bc56
seongcheol.yun <sungchul09@naver.com>
2023-01-24 15:52:36 +0900

    feat: dist 폴더 삭제
    gitignore 등록

--- typescript-ellie/motion/dist/components/page/page.js
+++
@@ -1,6 +0,0 @@
-import { BaseComponent } from './../component.js';
-export class PageComponent extends BaseComponent {
-    constructor() {
-        super('<ul class="page">This is PageComponent!</ul>');
-    }
-}
