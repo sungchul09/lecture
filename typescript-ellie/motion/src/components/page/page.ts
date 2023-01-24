f1bf47be6fb0afa00439a168cc1e00f1b098c1a6
sungchul09 <sungchul09@naver.com>
2023-01-24 15:33:40 +0900

    ellie-ts: 14.10 컴포넌트 리팩토링💡 (베이스 컴포넌트 만들기)

--- typescript-ellie/motion/src/components/page/page.ts
+++ typescript-ellie/motion/src/components/page/page.ts
@@ -1,14 +1,7 @@
-export class PageComponent {
-  // card 목록 
-  private element: HTMLUListElement;
+import { BaseComponent } from './../component.js';
+export class PageComponent extends BaseComponent<HTMLUListElement> {
   constructor() {
-    this.element = document.createElement('ul')
-    this.element.setAttribute('class', 'page')
-    this.element.textContent = 'This is PageComponent'
-  }
-
-  attachTo(parent: HTMLElement, position: InsertPosition = 'afterbegin') {
-    parent.insertAdjacentElement(position, this.element)
+    super('<ul class="page">This is PageComponent!</ul>')
   }
 }
 
