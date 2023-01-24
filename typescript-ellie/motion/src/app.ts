445befcf514e7eb03153fb882ac0d53c2fd5ba03
sungchul09 <sungchul09@naver.com>
2023-01-24 17:00:19 +0900

    ellie-ts: 14.17 페이지 아이템 컨테이너 만들기 💡

--- typescript-ellie/motion/src/app.ts
+++ typescript-ellie/motion/src/app.ts
@@ -1,25 +1,26 @@
+import { Component } from './components/component.js';
 import { NoteComponent } from './components/item/note.js';
 import { TodoComponent } from './components/item/todo.js';
 import { VideoComponent } from './components/item/video.js';
 import { ImageComponent } from './components/item/image.js'
-import { PageComponent } from './components/page/page.js'
+import { PageComponent, Composable } from './components/page/page.js'
 class App {
-  private readonly page: PageComponent
+  private readonly page: Component & Composable
   constructor(appRoot: HTMLElement) {
     this.page = new PageComponent()
     this.page.attachTo(appRoot)
 
     const image = new ImageComponent('Image Title', 'https://picsum.photos/600/300')
-    image.attachTo(appRoot, 'beforeend')
+    this.page.addChild(image)
     
     const note = new NoteComponent('Note Title', 'Note Body')
-    note.attachTo(appRoot, 'beforeend')    
+    this.page.addChild(note)
     
     const todo = new TodoComponent('Todo Title', 'Todo Item')
-    todo.attachTo(appRoot, 'beforeend')
+    this.page.addChild(todo)
     
     const video = new VideoComponent('Video Title', 'https://www.youtube.com/embed/VU2_yjIMDEg')
-    video.attachTo(appRoot, 'beforeend')
+    this.page.addChild(video)
   }
 }
 
