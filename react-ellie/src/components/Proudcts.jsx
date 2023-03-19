5763d8880f30c4834fa26ae21f796fb83133d3be
sungchul09 <sungchul09@naver.com>
2023-03-19 22:23:38 +0900

    feat: [REACT] useEffect 내부에 fetch 선언

--- react-ellie/src/components/Proudcts.jsx
+++ react-ellie/src/components/Proudcts.jsx
@@ -1,9 +1,20 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function Proudcts() {
   const [count, setcount] = useState(0);
   const [products, setProducts] = useState([]);
 
+  useEffect(() => {
+    fetch('data/products.json')
+      .then((res) => res.json())
+      .then((data) => {
+        console.log('뜨끈한 데이터를 받아옴!');
+        setProducts(data);
+      });
+    return () => {
+      console.log('clear');
+    };
+  }, []);
   return (
     <>
       <ul>
