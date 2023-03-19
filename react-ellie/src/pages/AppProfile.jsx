2afe71cad2470e2003446ceb776469d29cd50cc9
sungchul09 <sungchul09@naver.com>
2023-03-19 18:56:17 +0900

    feat: [REACT] props 처리

--- react-ellie/src/pages/AppProfile.jsx
+++ react-ellie/src/pages/AppProfile.jsx
@@ -1,11 +1,34 @@
 import './css/AppProfile.css';
 import Profile from '../components/Profile'
 function AppProfile() {
+  const profileMap = [
+    {
+      image:'https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80',
+      name: 'seongcheol',
+      title: '프론트엔드 개발자'
+    },
+    {
+      image:'https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80',
+      name: 'seongcheol2',
+      title: '백엔드 개발자'
+    },
+    {
+      image:'https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80',
+      name: 'seongcheol3',
+      title: 'devOps 개발자'
+    }
+  ]
   return (
   <>
-    <Profile />
-    <Profile />
-    <Profile />
+    {
+      profileMap.map(v => 
+        <Profile 
+          image={v.image}
+          name={v.name}
+          title={v.title}
+        />
+      )
+    }
   </>
   )
   
