554949fe228253ba9e49c0df4677defe763c48b2
sungchul09 <sungchul09@naver.com>
2023-03-19 22:29:05 +0900

    feat: [REACT] 어떤 특정 값이 요청될때 다시 네트워크 fetch 요청

--- react-ellie/src/components/Proudcts.jsx
+++ react-ellie/src/components/Proudcts.jsx
@@ -3,9 +3,10 @@ import React, { useEffect, useState } from 'react';
 export default function Proudcts() {
   const [count, setcount] = useState(0);
   const [products, setProducts] = useState([]);
-
+  const [checked, setChecked] = useState(false);
+  const handleChange = () => setChecked((prev) => !prev);
   useEffect(() => {
-    fetch('data/products.json')
+    fetch(`data/${checked ? 'sale_' : ''}products.json`)
       .then((res) => res.json())
       .then((data) => {
         console.log('뜨끈한 데이터를 받아옴!');
@@ -14,13 +15,15 @@ export default function Proudcts() {
     return () => {
       console.log('clear');
     };
-  }, []);
+  }, [checked]);
   return (
     <>
+      <input id="checkbox" type="checkbox" value={checked} onChange={handleChange} />
+      <label htmlFor="checkbox">show only hot sale</label>
       <ul>
         {
           products.map((product) => (
-            <li key={product.id}>
+            <li key={product.name}>
               <article>
                 <h3>{product.name}</h3>
                 <p>{product.price}</p>
