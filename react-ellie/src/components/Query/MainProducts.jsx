bdb51fbbeb5703e013f39b4dcd1e050d400db78f
sungchul09 <sungchul09@naver.com>
2023-03-20 03:03:58 +0900

    feat: [REACT] 리액트쿼리 useQueryClient 사용

--- react-ellie/src/components/Query/MainProducts.jsx
+++ react-ellie/src/components/Query/MainProducts.jsx
@@ -1,9 +1,11 @@
+import { useQueryClient } from '@tanstack/react-query';
 import Proudcts from 'components/Query/Proudcts';
 import React, { useState } from 'react';
 
 export default function MainProducts() {
   const [showLeftProducts, setShowLeftProducts] = useState(true);
   const [showRightProducts, setShowRightProducts] = useState(true);
+  const client = useQueryClient();
   return (
     <main className="container">
       <div>
@@ -14,6 +16,9 @@ export default function MainProducts() {
         {showRightProducts && <Proudcts />}
         <button type="button" onClick={() => setShowRightProducts((show) => !show)}>Toggle</button>
       </div>
+      <button type="button" onClick={() => { client.invalidateQueries(['products', false]); }}>
+        정보가 업데이트 되었음
+      </button>
     </main>
   );
 }
