56a7caeb121e6eb9a9c273ddd7bb4321f0525bc3
sungchul09 <sungchul09@naver.com>
2023-03-19 21:27:05 +0900

    feat: [REACT] 카운터 이벤트 넘기기

--- react-ellie/src/components/Counter.jsx
+++ react-ellie/src/components/Counter.jsx
@@ -1,17 +1,14 @@
 import React, { useState } from 'react'
 
-export default function Counter() {
+export default function Counter({total, onClick}) {
   const [count, setCount] = useState(0)
   return (
     <div className="counter">
-      <span className="number">{count}</span>
+      <span className="number">{count} <span className="total">/ {total}</span></span>
       <button className="button" onClick={() => {
         // setCount(count + 1)
         setCount((prev) => prev + 1)
-        setCount((prev) => prev + 1)
-        setCount((prev) => prev + 1)
-        setCount((prev) => prev + 1)
-        setCount((prev) => prev + 1)
+        onClick()
       }}>Add +</button>
     </div>
   )
--- react-ellie/src/pages/AppCounter.jsx
+++ react-ellie/src/pages/AppCounter.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Counter from '../components/Counter'
 import './css/AppCounter.css'
 
 export default function AppCounter() {
+  const [count, setCount] = useState(0)
+  const handleClick = () => setCount(prev => prev + 1)
   return (
-    <div>
-      <Counter />
+    <div class="counter-wrapper">
+      <div className="total-count">
+        TotalCount: {count} {count > 10 ? '🔥' : '💧'}
+      </div>
+      <Counter total={count} onClick={handleClick} />
+      <Counter total={count} onClick={handleClick} />
     </div>
   )
 }
