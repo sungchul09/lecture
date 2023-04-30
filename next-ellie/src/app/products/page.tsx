61747afb049e05d68b1d5b56f437521977e1d440
sungchul09 <sungchul09@naver.com>
2023-04-30 20:00:58 +0900

    feat: [NEXT] product 링크 추가

--- next-ellie/src/app/products/page.tsx
+++ next-ellie/src/app/products/page.tsx
@@ -1,7 +1,17 @@
+import Link from 'next/link'
 import React from 'react'
+
+const products = ['shirt', 'pants', 'skirt', 'shoes']
 
 export default function ProductsPage() {
   return (
-    <h1>ProductsPage</h1>
+    <>
+      <h1>제품 소개 페이지!</h1>
+      <ul>
+        { products.map((product, index) => <li key={index}>
+          <Link href={`products/${product}`}>{product}</Link>
+        </li>)}
+      </ul>
+    </>
   )
 }
