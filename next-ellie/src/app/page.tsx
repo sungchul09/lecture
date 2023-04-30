f965a3dbb53aa0d8ef0c8e223c1f9ec6f51e5650
sungchul09 <sungchul09@naver.com>
2023-04-30 22:13:56 +0900

    feat: [NEXT] 5.3 서버 컴포넌트 테스트

--- next-ellie/src/app/page.tsx
+++ next-ellie/src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import os from 'os' // node APIs
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  console.log(os.hostname())
   return (
     <h1>HomePage다</h1>
   )
