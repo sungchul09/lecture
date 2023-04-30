e13e424fac9307def27d1b9f6842da8e75fd1978
sungchul09 <sungchul09@naver.com>
2023-04-30 17:59:15 +0900

    feat: [NEXT] 4.9 페이지 미리 생성 generateStaticParams

--- next-ellie/src/app/products/[slug]/page.tsx
+++ next-ellie/src/app/products/[slug]/page.tsx
@@ -9,3 +9,10 @@ export default function SkirtsPage({params}: Props) { // props.params
     <h1>{params.slug} 페이지 입니다.</h1>
   )
 }
+
+export function generateStaticParams() {
+  const products = ['pants', 'skirts'];
+  return products.map(product => ({
+    slug: product
+  }))
+}
