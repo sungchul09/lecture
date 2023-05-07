bbfa2f1898221ffb523f5839c0f2ec60931c8d93
sungchul09 <sungchul09@naver.com>
2023-05-07 23:10:27 +0900

    feat: [NEXT] 컴포넌트화

--- next-ellie/src/app/products/page.tsx
+++ next-ellie/src/app/products/page.tsx
@@ -1,3 +1,4 @@
+import MeowArticle from '@/components/MeowArticle';
 import { getProducts } from '@/service/products'
 import Link from 'next/link'
 import React from 'react'
@@ -6,14 +7,18 @@ export const revalidate = 3;
 
 export default async function ProductsPage() {
   const products = await getProducts()
+
   return (
     <>
       <h1>제품 소개 페이지!</h1>
       <ul>
-        { products.map((product, index) => <li key={index}>
-          <Link href={`products/${product.id}`}>{product.name}</Link>
-        </li>)}
+        { products.map(({id, name}, index) => (
+        <li key={index}>
+          <Link href={`products/${id}`}>{name}</Link>
+        </li>
+        ))}
       </ul>
+      <MeowArticle />
     </>
   )
 }
---
+++ next-ellie/src/components/MeowArticle.tsx
@@ -0,0 +1,17 @@
+'use client'
+import React, { useEffect, useState } from 'react'
+import styles from './MeowArticle.module.css'
+
+export default async function MeowArticle() {
+  const [text, setText] = useState('데이터 준비중...')
+  useEffect(() => {
+    fetch('https://meowfacts.herokapp.com')
+    .then(res => res.json()
+    .then(data => setText(data.data[0])))
+  }, [])
+  return <>
+    <article className={styles.article}>
+        {text}
+    </article>
+  </>
+}
