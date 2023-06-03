ec16b6cfbe5763b9af27731c795fff99f02e9da9
sungchul09 <sungchul09@naver.com>
2023-06-03 18:00:34 +0900

    feat: [REDUX] devtools 추가

--- redux-mobx-zero/3.redux-in-react/App.jsx
+++ redux-mobx-zero/3.redux-in-react/App.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useCallback } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { logIn } from './actions/user'
  
 const App = () => {
+  const user = useSelector((state) => state.user.data)
+  const dispatch = useDispatch()
+  const onClick = useCallback(() => {
+    dispatch(logIn({
+      id: 'seongcheol',
+      password: '1234',
+    }))
+  }, [])
   return (
-    <div>
-      Hello, react-redux!
-    </div>
+    <>
+      <p>
+        user정보: {user ? user.nickName : <b>현재 유저 정보가 존재하지 않습니다.</b>}
+      </p>
+      <div>
+        <button onClick={onClick}>로그인</button>
+      </div>
+    </>
   )
 }
 
--- redux-mobx-zero/3.redux-in-react/client.jsx
+++ redux-mobx-zero/3.redux-in-react/client.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { hot } from 'react-hot-loader/root'
+import { Provider } from 'react-redux'
 
+import store from './store'
 import App from './App'
 
 const Hot = hot(App)
 
-ReactDOM.render(<Hot />, document.querySelector('#root'))
+ReactDOM.render(
+  <Provider store={store}>
+    <Hot />
+  </Provider>, 
+  document.querySelector('#root')
+)
