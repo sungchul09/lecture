05d032be5a62c8c94b33b7ed94cff3938c797433
sungchul09 <sungchul09@naver.com>
2023-12-15 00:30:19 +0900

    feat(fc-blog-app): 캐러셀, edit 퍼블리싱

--- fastcampus-blog-app/src/pages/posts/new.tsx
+++ fastcampus-blog-app/src/pages/posts/new.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import Header from '../../components/Header'
-import Footer from '../../components/Footer'
+import Header from 'components/Header'
+import Footer from 'components/Footer'
+import PostForm from 'components/PostForm'
 
 export default function PostNewPage() {
   return (
     <>
       <Header />
-      <div>Post Form</div>
+      <PostForm />
       <Footer />
     </>
   )
--- fastcampus-blog-app/src/pages/profile/index.tsx
+++ fastcampus-blog-app/src/pages/profile/index.tsx
@@ -1,8 +1,7 @@
-import React from 'react'
-import Profile from '../../components/Profile'
-import Header from '../../components/Header'
-import Footer from '../../components/Footer'
-import PostList from '../../components/PostList'
+import Profile from 'components/Profile'
+import Header from 'components/Header'
+import Footer from 'components/Footer'
+import PostList from 'components/PostList'
 
 export default function ProfilePage() {
   return (
