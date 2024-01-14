7021ca52ad4255424c72aa97d295dfe0b33371b7
sungchul09 <sungchul09@naver.com>
2024-01-14 22:16:08 +0900

    feat(fc-card-page): 카드 리스트 구현까지 완성

---
+++ fastcampus-card-page/src/App.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import HomePage from '@pages/Home'
+import TestPage from '@pages/Test'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" Component={HomePage} />
+        <Route path="/test" Component={TestPage} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ fastcampus-card-page/src/pages/Home.tsx
@@ -0,0 +1,21 @@
+import { useEffect } from 'react'
+
+import Top from '@components/shared/Top'
+
+import { getCards } from '@remote/card'
+import { getAdBanners } from '@/remote/adBanner'
+import AdBanners from '@/components/home/AdBanners'
+import CardList from '@/components/home/CardList'
+
+export default function HomePage() {
+  return (
+    <div>
+      <Top
+        title="혜택 좋은 카드"
+        subTitle="회원님을 위해서 혜택 좋은 카드를 모아봤어요"
+      ></Top>
+      <AdBanners />
+      <CardList />
+    </div>
+  )
+}
